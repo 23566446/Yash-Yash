b34a988578d38b1dec20e7b6ed924ba18dc82912
ttttsai <116952007+23566446@users.noreply.github.com>
2026-01-27 13:53:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI).then(() => console.log("✅ 成功連上 MongoDB!"));
 
@@ -440,4 +440,5 @@ app.put('/api/settings/marquee', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 YashYash 伺服器運作中: ${PORT}`));
+
 
